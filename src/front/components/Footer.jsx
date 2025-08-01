72306f5d03745fb16613e0bf6e510e8515437772
Vitoria Barbosa <vitoria.viva.bs@gmail.com>
2025-08-01 20:29:36 +0000

    updated foote linksr

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,18 +1,25 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 //assets
 import TasketLogo from "../assets/img/TasketLogo.png";
 
 export const Footer = () => (
+	
 	<footer className="footer py-3 footer_container py-5">
 		<div className="row mx-5 ps-5 ps-md-0">
-			<div className="col-12 col-md-8 d-block d-md-flex footer_links 
+			<div className="col-12 col-md-8 d-block d-md-flex  
 			align-items-center text-center text-md-start lh-1">
-				<p className="ms-md-1">Contact</p>
+				<Link to={"/contact"} className="text-decoration-none footer_links">
+					<p className="me-4 me-lg-0">Contact</p>
+				</Link>
 				<span className="footer_divider">|</span>
-				<p className="ps-2">Privacy Policy</p>
+				<Link to={"/privacy-policy"} className="text-decoration-none footer_links">
+					<p className="ps-2">Privacy Policy</p>
+				</Link>
 				<span className="footer_divider">|</span>
-				<p className="ps-2">Terms and Conditions</p>
+				<Link to={"/terms-conditions"} className="text-decoration-none footer_links">
+					<p className="ps-2">Terms and Conditions</p>
+				</Link>
 			</div>
 			<div className="col-12 col-md-4 d-block fs-4 text-center align-items-center">
 				<Link to="/">
