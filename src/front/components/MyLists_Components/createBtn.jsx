c1b5a82f2f5b470cd530f1d73415826f71c3adda
Vitoria Barbosa <vitoria.viva.bs@gmail.com>
2025-08-09 21:52:39 +0000

    updated modal

--- src/front/components/MyLists_Components/createBtn.jsx
+++ src/front/components/MyLists_Components/createBtn.jsx
@@ -16,9 +16,9 @@ export const CreateBtn = () => {
         
             <div className="modal fade" id="crateBtnModal" tabIndex="-1" aria-labelledby="createBtnModalLabel" aria-hidden="true">
                 <div className="modal-dialog">
-                    <div className="modal-content">
+                    <div className="modal-content modal_bg">
                         <div className="modal-header">
-                            <h5 className="modal-title" id="createBtnModalLabel">New List</h5>
+                            <h5 className="modal-title fw-bold fs-3 title_modal" id="createBtnModalLabel">New List</h5>
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
@@ -34,8 +34,11 @@ export const CreateBtn = () => {
                             </form>
                         </div>
                         <div className="modal-footer">
-                            <button type="button" className="btn btn-outline-dark fs-5" data-bs-dismiss="modal">Close</button>
-                            <button type="button" className="btn btn-success fs-5">Create</button>
+                            <button 
+                            type="button" 
+                            className="btn btn-outline-danger border-2 fs-5 fw-bold" 
+                            data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn_create fs-5">Create</button>
                         </div>
                     </div>
                 </div>
