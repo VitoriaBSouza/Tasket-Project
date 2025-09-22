344a02461d774c6fcd2adaaa3f7b2dc7283aba4b
Vitoria Barbosa <vitoria.viva.bs@gmail.com>
2025-09-22 19:19:20 +0000

    correct faq button on navbar

--- src/front/components/Navbar_Components/FaqButton.jsx
+++ src/front/components/Navbar_Components/FaqButton.jsx
@@ -9,7 +9,7 @@ export const FaqButton = () => {
 
 
     return (
-        <li className="nav-item mx-2 pb-1">
+        <li className="nav-item mx-2 pb-1 mt-3">
             <a className="nav-link mb-0 pb-0 ms-1" aria-current="page" href="/faq">
                 <FontAwesomeIcon icon={faCircleQuestion} className="icon_faq" />
             </a>
